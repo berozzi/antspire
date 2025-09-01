e94289337f2200bc49686a90c98a1688e0147754
Gracjan Gniot <gracjangniot@icloud.com>
2025-09-01 19:22:23 +0200

    Added basic camera movement - WASD and basic zoom

---
+++ Antspire/Assets/Scripts/Core/Managers/Camera/CameraMovement.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;// wymagane!
+using static UnityEditor.SceneView;
+
+public class CameraMovement : MonoBehaviour
+{
+    private PlayerControls controls;
+    private Vector2 moveInput;
+    private float zoomInput;
+
+    [Header("Ustawienia kamery")]
+    [SerializeField] float moveSpeed = 20f;
+    public Transform cameraTransform;
+    public float zoomSpeed = 20f;
+    public float minZoom = 5f;
+    public float maxZoom = 50f;
+
+    [SerializeField] private Camera cam;
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+        // Podpinamy eventy pod akcje
+        controls.Camera.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        controls.Camera.Move.canceled += ctx => moveInput = Vector2.zero;
+
+        controls.Camera.Zoom.performed += ctx => zoomInput = ctx.ReadValue<float>();
+        controls.Camera.Zoom.canceled += ctx => zoomInput = 0f;
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    private void Update()
+    {
+        // Ruch kamery po płaszczyźnie
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y) * moveSpeed * Time.deltaTime;
+        transform.Translate(move, Space.World);
+
+        Zoom();
+
+    }
+
+    void Zoom()
+    {
+        float oldSize = cam.orthographicSize;
+        cam.orthographicSize -= zoomInput * zoomSpeed * Time.deltaTime;
+        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, minZoom, maxZoom);
+        float sizeDiff = oldSize - cam.orthographicSize;
+        if (Mathf.Abs(zoomInput) > 0.001f)
+        {
+            Vector3 cursorWorldPos = cam.ScreenToWorldPoint(
+            new Vector3(Mouse.current.position.x.ReadValue(),
+                Mouse.current.position.y.ReadValue(),
+                cam.nearClipPlane)
+            );
+
+            // kierunek do kursora na płaszczyźnie XY/Z
+            Vector3 direction = (cursorWorldPos - cameraTransform.position).normalized;
+
+            // przesuwasz kamerę przy zoomie
+            cameraTransform.position += direction * sizeDiff * zoomSpeed * Time.deltaTime;
+            // Scroll w górę zmniejsza orthographicSize  przybliża
+            
+        }
+    }
+}
