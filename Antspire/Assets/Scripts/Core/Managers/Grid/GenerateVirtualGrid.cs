aca83b28d72e2763041ad1fb2b187b819dd7cbf4
Gracjan Gniot <135625848+berozzi@users.noreply.github.com>
2025-10-22 16:37:38 +0200

    Added GetCell method
    This function is used for checking recevied coordinates if they are in grid

--- Antspire/Assets/Scripts/Core/Managers/Grid/GenerateVirtualGrid.cs
+++ Antspire/Assets/Scripts/Core/Managers/Grid/GenerateVirtualGrid.cs
@@ -1,21 +1,34 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GenerateVirtualGrid : MonoBehaviour
 {
-    public int width = 50;
-    public int height = 50;
+    [SerializeField ]int width = 51;
+    [SerializeField ]int height = 51; // because we start from 1 in loops
     float cellSize = 1f; // rozmiar pola w œwiecie
 
-    Cell[,] grid;
+    public Cell[,] grid;
 
     void Start()
     {
+        InitializeGrid();
+    }
+
+    public void InitializeGrid()
+    {
+        if (width <= 0 || height <= 0)
+        {
+            Debug.LogError($"Grid dimensions must be positive! Current: {width}x{height}");
+            width = Mathf.Max(1, width);
+            height = Mathf.Max(1, height);
+        }
         grid = new Cell[width, height];
 
-        for (int x = 0; x < width; x++)
+        for (int x = 1; x < width; x++)
         {
-            for (int y = 0; y < height; y++)
+            for (int y = 1; y < height; y++)
             {
+                
                 grid[x, y] = new Cell
                 {
                     position = new Vector2Int(x, y),
@@ -24,7 +37,6 @@ public class GenerateVirtualGrid : MonoBehaviour
             }
         }
     }
-
     // konwersja ze œwiata 3D -> grid
     public Vector2Int WorldToGrid(Vector3 worldPos)
     {
@@ -36,6 +48,16 @@ public class GenerateVirtualGrid : MonoBehaviour
     // odwrotnie: grid -> pozycja w œwiecie
     public Vector3 GridToWorld(Vector2Int gridPos)
     {
-        return new Vector3(gridPos.x * cellSize, 0, gridPos.y * cellSize);
+        var pos =  new Vector3(gridPos.x * cellSize, 0, gridPos.y * cellSize);
+        return pos;
+    }
+    public Cell GetCell(Vector2Int coord)
+    {
+        // SprawdŸ granice siatki
+        if (coord.x >= 0 && coord.x < width && coord.y >= 0 && coord.y < height) // w GenerateVirtualGrid szerokoœæ i wysokoœæ s¹ ustawione na 50
+        {
+            return grid[coord.x, coord.y];
+        }
+        return null;
     }
 }
